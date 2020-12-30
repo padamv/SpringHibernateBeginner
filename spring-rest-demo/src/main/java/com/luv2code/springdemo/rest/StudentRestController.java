ed8828bbca6136ceeb0663b1c78652cf7f2a231e
padamv <padamv91@gmail.com>
2020-12-30 11:30:22 +0100

    Create ExceptionHandler for text id format

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -75,5 +75,24 @@ public class StudentRestController {
 		
 		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
 	}
+	
+	// add another exception handler ... to  catch any exception (catch all)
+	
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
+		
+		// create a StudentErrorResponse
+		
+				StudentErrorResponse error = new StudentErrorResponse();
+				
+				error.setStatus(HttpStatus.BAD_REQUEST.value());
+				error.setMessage(exc.getMessage());
+				error.setTimeStamp(System.currentTimeMillis());
+				
+				// return ResponseEntity
+				
+				return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+		
+	}
 
 }
