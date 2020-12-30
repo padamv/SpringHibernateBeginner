37db4b7a100ade9fdbeb0111232635b50e538f74
padamv <padamv91@gmail.com>
2020-12-30 11:21:31 +0100

    Creating exception handler

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -53,6 +56,24 @@ public class StudentRestController {
 		}
 		
 		return theStudents.get(studentId);
+	}
+	
+	// Add an exception handler using @ExceptionHandler
+	
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+		
+		// create a StudentErrorResponse
+		
+		StudentErrorResponse error = new StudentErrorResponse();
+		
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		// return ResponseEntity
+		
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
 	}
 
 }
