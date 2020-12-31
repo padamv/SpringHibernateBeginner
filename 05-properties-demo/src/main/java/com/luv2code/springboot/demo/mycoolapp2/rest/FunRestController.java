f0a4858aa301ceb117469673406ce60f0fc21903
padamv <padamv91@gmail.com>
2020-12-31 10:46:10 +0100

    Show properties with REST

--- 05-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp2/rest/FunRestController.java
+++ 05-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp2/rest/FunRestController.java
@@ -2,11 +2,27 @@ package com.luv2code.springboot.demo.mycoolapp2.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+	
+	// inject properties  for: coach.name and team.name
+	
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamName;
+	
+	// expose new endpoint for "teaminfo"
+	
+	@GetMapping("/teaminfo")
+	public String getTeamInfo() {
+		return "Coach: " + coachName + ", Team: " + teamName;
+	}
 
 	// expose "/" that return "Hello World"
 	
