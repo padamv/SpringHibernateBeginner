747f49263a524d035fa272c88a52068ce19ac242
padamv <padamv91@gmail.com>
2020-12-31 12:49:47 +0100

    Add post employees to REST (create new employee)

--- cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,30 +18,43 @@ import com.luv2code.springboot.cruddemo.service.EmployeeService;
 public class EmployeeRestController {
 
 	private EmployeeService employeeService;
-	
+
 	// quick and dirty: inject employee dao (Use constructor injection)
 	@Autowired
 	public EmployeeRestController(EmployeeService theEmployeeService) {
 		employeeService = theEmployeeService;
 	}
-	
-	//expose "/employees" and return list of employee
+
+	// expose "/employees" and return list of employee
 	@GetMapping("/employees")
 	public List<Employee> findAll() {
 		return employeeService.findAll();
 	}
-	
+
 	// add mapping for GET /employees/{employeeId}
-	
+
 	@GetMapping("/employees/{employeeId}")
 	public Employee getEmployee(@PathVariable int employeeId) {
-		
+
 		Employee theEmployee = employeeService.findById(employeeId);
-		
-		if(theEmployee == null) {
+
+		if (theEmployee == null) {
 			throw new RuntimeException("Employee id not found - " + employeeId);
 		}
-		
+
+		return theEmployee;
+	}
+
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+
+		// also just in case they pass an id in JSON ... set id to 0 (it will
+		// ensure inserting)
+
+		theEmployee.setId(0);
+
+		employeeService.save(theEmployee);
+
 		return theEmployee;
 	}
 }
