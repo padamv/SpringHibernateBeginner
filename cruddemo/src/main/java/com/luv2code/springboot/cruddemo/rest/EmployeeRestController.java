2655dfb29376a8a482532d07e30a3211acc3bf16
padamv <padamv91@gmail.com>
2020-12-31 12:04:50 +0100

    Add RestController for employees (get)

---
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,30 @@
+package com.luv2code.springboot.cruddemo.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
+import com.luv2code.springboot.cruddemo.entity.Employee;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+	private EmployeeDAO employeeDAO;
+	
+	// quick and dirty: inject employee dao (Use constructor injection)
+	@Autowired
+	public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
+		employeeDAO = theEmployeeDAO;
+	}
+	
+	//expose "/employees" and return list of employee
+	@GetMapping("/employees")
+	public List<Employee> findAll() {
+		return employeeDAO.findAll();
+	}
+}
