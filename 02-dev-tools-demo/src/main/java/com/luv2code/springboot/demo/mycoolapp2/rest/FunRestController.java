133814d89f0ec389fc622ac5a7829e7f38d7361f
padamv <padamv91@gmail.com>
2020-12-31 08:56:38 +0100

    Adding new endpoints (/workout and /fortune)

--- 02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp2/rest/FunRestController.java
+++ 02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp2/rest/FunRestController.java
@@ -14,4 +14,16 @@ public class FunRestController {
 	public String sayHello() {
 		return "Hello World! Time on server is: " + LocalDateTime.now();
 	}
+	
+	// expose a new endpoint for "workout"
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		return "Run a hard 5k!";
+	}
+	
+	// expose a new endpoint for fortune
+	@GetMapping("/fortune")
+	public String getDailyFortune() {
+		return "Today is your lucky day!";
+	}
 }
