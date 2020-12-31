c4fb2bcb5ad0a7007c2aeb1a59908f8053f558d8
padamv <padamv91@gmail.com>
2020-12-31 09:01:51 +0100

    Creating project from dev-tools-demo

---
+++ 03-actuator-demo/src/test/java/com/luv2code/springboot/demo/mycoolapp2/Mycoolapp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.demo.mycoolapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mycoolapp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
