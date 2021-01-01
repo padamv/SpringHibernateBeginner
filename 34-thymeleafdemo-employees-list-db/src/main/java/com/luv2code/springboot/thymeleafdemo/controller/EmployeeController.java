36bedece464fc3c0f7f006d9426b54fa19e2dd6f
padamv <padamv91@gmail.com>
2021-01-01 11:54:53 +0100

    Update POM, properties and add employee entity for db

--- 34-thymeleafdemo-employees-list-db/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 34-thymeleafdemo-employees-list-db/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -10,7 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.luv2code.springboot.thymeleafdemo.model.Employee;
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 
 @Controller
 @RequestMapping("/employees")
--- 34-thymeleafdemo-employees-list-db/src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Employee.java
+++ 34-thymeleafdemo-employees-list-db/src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Employee.java
@@ -1,22 +1,52 @@
-package com.luv2code.springboot.thymeleafdemo.model;
+package com.luv2code.springboot.thymeleafdemo.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
 public class Employee {
 
+	// define fields
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
 	private int id;
+	
+	@Column(name="first_name")
 	private String firstName;
+	
+	@Column(name="last_name")
 	private String lastName;
+	
+	@Column(name="email")
 	private String email;
+	
+	// define constructors
 	
 	public Employee() {
 		
 	}
 
+	public Employee(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	
 	public Employee(int id, String firstName, String lastName, String email) {
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 	}
+
+	// define getters/setters
 
 	public int getId() {
 		return id;
@@ -50,10 +80,11 @@ public class Employee {
 		this.email = email;
 	}
 
+	// define toString
+	
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName="
 				+ lastName + ", email=" + email + "]";
 	}
-	
 }
