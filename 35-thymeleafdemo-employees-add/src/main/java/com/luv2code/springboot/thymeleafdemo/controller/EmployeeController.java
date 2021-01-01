2c5e52567ad8e1346e8edec34d63fb94bd8c2eb1
padamv <padamv91@gmail.com>
2021-01-01 12:58:33 +0100

    Add save employee to controller

--- 35-thymeleafdemo-employees-add/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 35-thymeleafdemo-employees-add/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
@@ -46,5 +48,15 @@ public class EmployeeController {
 		theModel.addAttribute("employee", theEmployee);
 		
 		return "employees/employee-form";
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+		
+		// save the employee
+		employeeService.save(theEmployee);
+		
+		// use a redirect to prevent duplicate submissions
+		return "redirect:/employees/list";
 	}
 }
