2e8c847a39523bcd9bdd6605b734610d5980d29a
padamv <padamv91@gmail.com>
2021-01-01 12:28:23 +0100

    Project created from employees-list

---
+++ 35-thymeleafdemo-employees-add/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,38 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+	
+	private EmployeeService employeeService;
+	
+	@Autowired
+	public EmployeeController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
+	}
+	
+	// add mapping for "/list"
+	
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+		
+		// get the employees from the database
+		List<Employee> theEmployees = employeeService.findAll();
+		
+		//add to the spring model
+		theModel.addAttribute("employees", theEmployees);
+		
+		return "list-employees";
+	}
+}
