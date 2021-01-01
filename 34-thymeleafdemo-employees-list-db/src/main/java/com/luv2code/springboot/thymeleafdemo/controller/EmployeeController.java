2b8aad1b85b3b54031eb0630daabedb94811746d
padamv <padamv91@gmail.com>
2021-01-01 12:19:12 +0100

    Add REST controller to list employees from DB

--- 34-thymeleafdemo-employees-list-db/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 34-thymeleafdemo-employees-list-db/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,46 +1,34 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 	
-	// load employee data
+	private EmployeeService employeeService;
 	
-	private List<Employee> theEmployees;
-	
-	@PostConstruct
-	private void loadData() {
-		
-		// create employees
-		Employee empl1 = new Employee(1, "Leslie", "Andrews", "leslie@luv2code.com");
-		Employee empl2 = new Employee(2, "Emma", "Baumgarten", "emma@luv2code.com");
-		Employee empl3 = new Employee(3, "Avani", "Gupta", "avani@luv2code.com");
-		
-		// create the list
-		theEmployees = new ArrayList<>();
-		
-		// add to the list
-		theEmployees.add(empl1);
-		theEmployees.add(empl2);
-		theEmployees.add(empl3);
+	@Autowired
+	public EmployeeController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
 	}
 	
 	// add mapping for "/list"
 	
 	@GetMapping("/list")
 	public String listEmployees(Model theModel) {
+		
+		// get the employees from the database
+		List<Employee> theEmployees = employeeService.findAll();
 		
 		//add to the spring model
 		theModel.addAttribute("employees", theEmployees);
